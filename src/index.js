6f67f9d42ba6fd54b00ff8e685b13e4cf3ad026a
utopastac <becter@peters-mbp.lan>
2021-10-31 16:06:37 +0000

    Adding hasrouter because why not

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import './index.module.sass';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router basename={'/docs'}>
+  <Router basename={'/'}>
     <App />
   </Router>,
   document.getElementById('root')
